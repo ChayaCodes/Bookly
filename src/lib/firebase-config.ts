d991c09ad78408b3df0fc781cf6a865ad3963bdb
Chaya Kremer <chaya41182@gmail.com>
2025-08-03 16:18:03 +0000

    File changes

--- src/lib/firebase-config.ts
+++ src/lib/firebase-config.ts
@@ -3,10 +3,10 @@
 // with your actual Firebase project's configuration.
 
 export const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
+  "projectId": "bookly-4mfhs",
+  "appId": "1:924943845303:web:bd817597b2ae60d1918a32",
+  "storageBucket": "bookly-4mfhs.appspot.com",
+  "apiKey": "AIzaSyCFpdWIrHEy7CHc8lqcUxbykZsukTBNjy4",
+  "authDomain": "bookly-4mfhs.firebaseapp.com",
+  "messagingSenderId": "924943845303"
 };
