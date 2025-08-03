707b947f0f26c9bef48ecb4c65960d4de5d5dd68
Chaya Kremer <chaya41182@gmail.com>
2025-08-03 21:05:19 +0000

    נפתח לי תצוגה שנראית ככה, נראה שזה לא תומך עברית

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,7 @@ export default {
       fontFamily: {
         body: ['PT Sans', 'sans-serif'],
         headline: ['Playfair Display', 'serif'],
-        code: ['monospace'],
+        code: ['"Source Code Pro"', 'monospace'],
       },
       colors: {
         background: 'hsl(var(--background))',
